10840193dddf9f88b454c0b4adff897a3f871ec1
Kajal Sehrawat <63706261+kajal23sehrawat@users.noreply.github.com>
2023-03-19 08:38:27 -0400

    alpha using bisection method, segment length

--- controller.py
+++ controller.py
@@ -1,14 +1,15 @@
 from lib.trignometry import TrignometryWBI
 from lib.math_lib import MathLib_WBI
 
+
 R = float(input("Enter the radius: "))
+    
+
 twbi = TrignometryWBI()
 mwbi = MathLib_WBI()
 
-
 def f_value(alpha):
     return alpha - twbi.sin(alpha) - mwbi.get_pi()/2
-
 
 # BISECTION METHOD
 
@@ -25,14 +26,11 @@ while b - a > tolerance:
         a = c
 
 alpha = (a + b) / 2
+print("The value of alpha is: ", alpha)
 
 # Calculate the length of the segment X1X2
 
 l = 2*R*(1 - twbi.cos(alpha/2))
-
-print("The value of alpha is: ", alpha)
-
 print("The length of segment is: ", l)
 
-# Print the result
 print(f"The two coasters need to be moved {l:.4f} units on top of each other.")
