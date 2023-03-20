a15105814454b109e738d5d3e3917c8156002c6f
Nishant Saini <ni_saini@live.concordia.ca>
2023-03-20 12:42:44 -0400

    Added pydoc

--- lib/root_approximation.py
+++ lib/root_approximation.py
@@ -9,6 +9,16 @@ class RootApproximation_WBI(RootApproximation):
         super().__init__()
 
     def get_roots(self, func, e) -> list:
+        """
+        Calculates the root of a given function using the secant method with the given precision.
+
+        Args:
+            func (function): The function to calculate the root of.
+            e (float): The precision of the root. This parameter is not used.
+
+        Returns:
+            list: The root of the function.
+        """
         x0, x1 = 0, 99
         x2 = 0
         step = 1
@@ -39,5 +49,15 @@ class RootApproximation_BI(RootApproximation):
         self.num_terms = 100
 
     def get_roots(self, func, e) -> list:
+        """
+        Calculates the root of a given function using the `fsolve` method.
+
+        Args:
+            func (function): The function to calculate the root of.
+            e (float): The precision of the root.
+
+        Returns:
+            list: The root of the function.
+        """
         root = fsolve(func, 0)
         return root
